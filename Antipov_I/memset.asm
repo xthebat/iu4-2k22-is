2f97b3c75e103b56ac5515146a104e0c3495aad4
atvf000 <47750049+atvf000@users.noreply.github.com>
2022-02-24 17:38:05 +0300

    start 1st task

---
+++ Antipov_I/memset.asm
@@ -0,0 +1,14 @@
+
+SECTION .text 
+; void * memset( void * memptr, int val, size_t num );
+memset:
+    mov     rax, rcx 
+memset_cyc:
+    test    r8, r8
+    jz      memset_ret
+    sub     r8, 1
+    mov     byte [rcx], dl
+    add     rcx, 1
+    jmp     memset_cyc
+memset_ret:
+    ret
