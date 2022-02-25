0c5afd9d42f46630cde6e85abdc527e6eba4f4ec
atvf000 <47750049+atvf000@users.noreply.github.com>
2022-02-25 21:45:29 +0300

    misc fix

--- Antipov_I/task01/strstr.asm
+++ Antipov_I/task01/strstr.asm
@@ -5,9 +5,10 @@ strstr:
     push    r11 
     push    r12  
     push    r13
+    push    r14
 
     xor     rax, rax
-    xor     r10b, r10b
+    xor     r10, r10
     mov     r12b, byte [rdx]
     test    r12b, r12b
     jnz     strstr_global_loop
@@ -15,14 +16,14 @@ strstr:
     jmp     strstr_end
          
 strstr_global_loop:
-    xor     r11b, r11b
+    xor     r11, r11
 strstr_inner_loop:
     ; substr ends
     mov     r12b, byte [rdx + r11] 
     test    r12b, r12b
     jnz     strstr_substr_null
     mov     rax, rcx 
-    add     rax, r10b
+    add     rax, r10
     jmp     strstr_end 
 
 strstr_substr_null:
@@ -34,20 +35,21 @@ strstr_substr_null:
     jmp     strstr_end
 
 strstr_str_null:
-    mov     r12b, r11b
-    add     r12b, r10b
-    add     r10b, 1
-    mov     r13b, byte [rcx + r12]
-    mov     r12b, byte [rdx + r12]
+    mov     r14, r11
+    add     r14, r10
+    add     r10, 1
+    mov     r13b, byte [rcx + r14]
+    mov     r12b, byte [rdx + r14]
     test    r12b, r13b
     jne     strstr_next_symb
     add     r11b, 1
     jmp     strstr_inner_loop
 
 strstr_next_symb:
-    add     r10b, 1
+    add     r10, 1
     jmp     strstr_global_loop
 strstr_end:
+    pop     r14
     pop     r13
     pop     r12
     pop     r11
