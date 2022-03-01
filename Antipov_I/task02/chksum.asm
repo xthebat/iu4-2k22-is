860759ede9c317e87d7089fad986f76dd969f1fb
atvf000 <47750049+atvf000@users.noreply.github.com>
2022-03-01 19:24:30 +0300

    complete task02

---
+++ Antipov_I/task02/chksum.asm
@@ -0,0 +1,18 @@
+SECTION .text
+;int chksum(uint8_t* buf, int size)
+chksum:
+    push    r8
+    xor     rax, rax
+chksum_loop:
+    test    rdx, rdx
+    jz      chksum_ret
+    movzx   r8, byte [rcx]
+    add     rax, r8
+    add     rcx, 1
+    sub     rdx, 1
+    jmp     chksum_loop
+
+chksum_ret:
+    pop     r8
+    ret
+
---
+++ Antipov_I/task02/strncpy.asm
@@ -0,0 +1,28 @@
+SECTION .text 
+;char * strncpy( char * destptr, const char * srcptr, size_t num );
+strncpy:
+    mov     rax, rcx
+    test    r8, r8
+    jz      strncpy_ret
+    push    r9
+
+strncpy_loop:
+    mov     r9b, byte[rcx]              ; if *destptr == '\0'
+    test    r9b, r9b
+    jz      strncpy_ret
+    add     rcx, 1 
+
+    mov     r9b, byte[rdx]              ; if *srcptr == '\0'
+    test    r9b, r9b
+    jz      strncpy_skip
+    add     rdx, 1
+
+strncpy_skip:
+    sub     r8, 1
+    test    r8,r8
+    jz      strncpy_ret
+    jmp     strncpy_loop
+       
+strncpy_ret:
+    pop r9
+    ret
