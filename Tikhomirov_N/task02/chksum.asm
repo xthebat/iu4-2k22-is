224e1abe9f21d20648f0d46d468f92510a135de0
NickTikhomirov <47750222+NickTikhomirov@users.noreply.github.com>
2022-03-06 22:13:46 +0300

    Task02 (#6)
    * initial commit for task02
    
    * сделал делание
    
    * починил своё лицо и сделал int интом :(
    
    * по совету опытных комрадов третьего дня...

---
+++ Tikhomirov_N/task02/chksum.asm
@@ -0,0 +1,26 @@
+;int chksum(uint8_t* buf, int size) {
+;    int result = 0;
+;    for (int k = 0; k < size; k++) {
+;        result += buf[k];
+;    }
+;    return result;
+;}
+
+; rcx - array of bytes
+; rdx - size
+
+
+SECTION .text
+chksum:
+    xor eax, eax                      ; чистим регистр (best practice)
+    chksum_cycle:
+        test edx, edx                 ; критерий выхода из цикла
+        jz chksum_finish
+            movzx r8d, byte [rcx]      ; надо сложить x8 и x32, поэтому используем буфер ; movzx is awesome also
+            add eax, r8d              ; (собсна выкладываем из буфера и делаем сложение)
+            add rcx, 1                ; двигаем указатель
+            sub edx, 1                ; уменьшаем счётчик
+            jmp chksum_cycle          ; цикл
+    chksum_finish:
+ret
+; как говорил Жуков Алексей Евгеньевич, это задача для маленьких сопливых девочек
