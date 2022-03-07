ffdb53cc99769ce41dcd7aa4937b3ccf894cc740
TarakanovaDaria <Dashonok1999@yandex.ru>
2022-03-07 12:11:36 +0300

    Tarakanova D task01

--- Tarakanova_D/task01/chksum.asm
+++ Tarakanova_D/task01/chksum.asm
@@ -4,19 +4,23 @@
 chksum:
 	push rbx
 	mov rbx, rcx			; rbx = rcx
+	mov r9d, 0				; (int)r9d = 0
 	
+	test edx, edx			; edx <> 0
+	jz exit					; if (edx == 0) goto exit <==> zf == 1
+
 loop:
-	movzx r8d, byte [rbx] 	; r8 = *(uint8_t*)rbx
-	add r9d, r8d		 	; r9d += r8d
+	
+	movzx r8d, byte [rbx] 	; (int)r8 = *(uint8_t*)rbx
+	add r9d, r8d		 	; (int)r9d += (int)r8d
  
 	add rbx, 1 				; rbx += 1
 	sub edx, 1 				; edx -= 1  
 	cmp edx, 0				; edx <> 0
 	jz loop 				; if (zf == 0) goto loop
-
-	pop rbx  				; <restore rbx>
 	
 exit:
-	mov eax, r9d				; return r9d
+	pop rbx  				; <restore rbx>
+	mov eax, r9d			; return r9d
 	ret 
 	
