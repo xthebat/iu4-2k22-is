e11c38a09c4f116f04932a0f4fc7612a32b6f410
Toliak <ibolosik@gmail.com>
2022-03-21 16:16:01 +0300

    [Task04] WIP on key generator

--- Titov_A/task04/resolver_rust/src/main.rs
+++ Titov_A/task04/resolver_rust/src/main.rs
@@ -1,10 +1,57 @@
-use clap::{Arg, Command};
+extern crate core;
+
+use clap::{Arg, ArgMatches, Command};
+use std::num::ParseIntError;
+use std::panic;
+
+trait PathTrait {
+    fn calc_path_chksum(&self) -> u16;
+}
+
+impl PathTrait for String {
+    fn calc_path_chksum(&self) -> u16 {
+        0x1004 // TODO: implement
+    }
+}
+
+#[derive(Debug)]
+struct AppData {
+    dir: String,
+    sleep: u8,
+    request_id: u8,
+}
+
+impl AppData {
+    pub fn prepare_key(&self) -> u32 {
+        let sleep_result = 0x10 - (self.sleep & 0xF);
+        ((sleep_result as u32 & 0xF) << 0x18)
+            | ((0xD) << 0x14)
+            | ((self.dir.calc_path_chksum() as u32 & 0xFFFF) << 4)
+            | (self.request_id as u32 & 0xF)
+    }
+}
+
+enum ArgKeys {
+    Dir,
+    RequestId,
+    Sleep,
+}
+
+impl ArgKeys {
+    fn as_str(&self) -> &'static str {
+        match self {
+            ArgKeys::Dir => "dir",
+            ArgKeys::RequestId => "requestid",
+            ArgKeys::Sleep => "sleep",
+        }
+    }
+}
 
 fn main() {
     let matches = Command::new("Task04 Resolver")
         .version("1.0.0")
         .arg(
-            Arg::new("dir")
+            Arg::new(ArgKeys::Dir.as_str())
                 .short('d')
                 .long("dir")
                 .takes_value(true)
@@ -12,7 +59,7 @@ fn main() {
                 .help("Directory with task04.exe"),
         )
         .arg(
-            Arg::new("sleep")
+            Arg::new(ArgKeys::Sleep.as_str())
                 .short('s')
                 .long("sleep")
                 .takes_value(true)
@@ -20,7 +67,7 @@ fn main() {
                 .help("Sleep time (seconds)"),
         )
         .arg(
-            Arg::new("requestid")
+            Arg::new(ArgKeys::RequestId.as_str())
                 .short('r')
                 .long("requestid")
                 .takes_value(true)
@@ -29,6 +76,19 @@ fn main() {
         )
         .get_matches();
 
-    let sleep_secs = matches.value_of("sleep");
-    println!("Hello, world!");
+    let data: AppData = AppData {
+        dir: String::from(matches.value_of(ArgKeys::Dir.as_str()).unwrap()),
+        sleep: matches
+            .value_of(ArgKeys::Sleep.as_str())
+            .unwrap()
+            .parse::<u8>()
+            .unwrap(),
+        request_id: matches
+            .value_of(ArgKeys::RequestId.as_str())
+            .unwrap()
+            .parse::<u8>()
+            .unwrap(),
+    };
+
+    println!("0x{:08X}", data.prepare_key());
 }
